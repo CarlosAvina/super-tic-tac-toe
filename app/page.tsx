4f4847e8a2cc6bd36c48535b361a919f20868778
Carlos Avina <carlosavilu11@gmail.com>
2023-12-21 17:23:24 -0600

    WIP

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,14 @@
 import Link from "next/link";
 export default function Home() {
-  return <Link href="/play/asdjk">Play</Link>;
+  return (
+    <main className="w-full flex flex-col justify-center items-center text-center h-screen gap-5">
+      <h1 className="text-4xl font-bold">Super tic tac toe</h1>
+      <Link className="p-3 bg-green-300 rounded-lg" href="/play">
+        Local
+      </Link>
+      <Link className="p-3 bg-green-300 rounded-lg" href="/play/asdjk">
+        Online
+      </Link>
+    </main>
+  );
 }
